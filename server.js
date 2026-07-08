159f644a735e864cdaff37e3f0ab125e08aaf996
Daniele Melo Nunes <danielemelonunes@192.168.1.246>
2026-07-08 16:53:18 -0300

    adicionando pagina index.html

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    res.send(`
-        <h1>Aplicação Node.js</h1>
-        <p>Servidor rodando na porta ${PORT}</p>
-    `);
+// Permite servir arquivos estáticos
+app.use(express.static('public'));
+
+// API que retorna as informações
+app.get('/api', (req, res) => {
+    res.json({
+        application: "Node.js",
+        status: "Running",
+        port: PORT,
+        serverTime: new Date().toLocaleString(),
+        environment: process.env.NODE_ENV || "Development"
+    });
 });
 
 app.listen(PORT, () => {
