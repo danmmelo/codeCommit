3c7574095a15838a4a91ffd3389b58a64f251d76
Daniele Melo Nunes <danielemelonunes@MacBook-Air-2.local>
2026-09-01 16:41:56 -0300

    adição do SQS_QUEUE_URL_PARAM

--- services/sqsService.js
+++ services/sqsService.js
@@ -1,10 +1,30 @@
 const { SQSClient, SendMessageCommand } = require("@aws-sdk/client-sqs");
+const { getParameter } = require("./parameterStore");
 
 const client = new SQSClient({ region: process.env.AWS_REGION });
 
+let cachedQueueUrl = null;
+
+async function getQueueUrl() {
+
+  if (cachedQueueUrl) {
+    return cachedQueueUrl;
+  }
+
+  const param = await getParameter(process.env.SQS_QUEUE_URL_PARAM, false);
+
+  cachedQueueUrl = param.Value;
+
+  return cachedQueueUrl;
+
+}
+
 async function sendSecretCreatedMessage(secretName) {
+
+  const queueUrl = await getQueueUrl();
+
   const command = new SendMessageCommand({
-    QueueUrl: process.env.SECRET_CREATED_QUEUE_URL,
+    QueueUrl: queueUrl,
     MessageBody: JSON.stringify({
       event: "SecretCreated",
       secretName,
@@ -13,6 +33,7 @@ async function sendSecretCreatedMessage(secretName) {
   });
 
   return client.send(command);
+
 }
 
 module.exports = { sendSecretCreatedMessage };
